392b6f44a00002c5666ef14acf5078ec37a7055a
gemarico <gemarico@outlook.it>
2019-12-07 17:56:41 +0100

    Project almost finished, change target image

---
+++ Assets/SantaScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SantaScript : MonoBehaviour {
+
+    public GameObject Santa;
+    public AudioClip aud0;
+    public AudioClip aud1;
+    public AudioClip aud2;
+
+    private Animator animator;
+
+    void Start()
+    {
+        animator = Santa.GetComponent<Animator> ();
+       
+    }
+
+   
+
+    public void Canta()
+    {
+        animator.SetTrigger("canta");
+        animator.gameObject.GetComponent<AudioSource>().clip = aud1;
+        animator.gameObject.GetComponent<AudioSource>().Play();
+    }
+
+    public void Baila()
+    {
+        animator.SetTrigger("baila");
+        animator.gameObject.GetComponent<AudioSource>().clip = aud2;
+        animator.gameObject.GetComponent<AudioSource>().Play();
+    }
+
+}
